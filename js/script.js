4ce309f98243792b0695704dc7779385977321be
NatasjaBorchJensen <natasjaborch@gmail.com>
2026-06-02 12:33:23 +0200

    responsiv

--- js/script.js
+++ js/script.js
@@ -148,3 +148,18 @@ window.addEventListener("scroll", () => {
     });
 
 
+/* ===== BURGER MENU ===== */
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+if (hamburger && navLinks) {
+
+    hamburger.addEventListener("click", () => {
+
+        navLinks.classList.toggle("active");
+
+    });
+
+}
+
